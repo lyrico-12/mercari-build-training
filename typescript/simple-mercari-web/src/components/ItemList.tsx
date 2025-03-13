2bf3315e5874d053267d22e0383395c1660abee3
lyrico-12 <lyrico1202@gmail.com>
2025-03-14 00:08:37 +0900

    searchの実装途中

--- typescript/simple-mercari-web/src/components/ItemList.tsx
+++ typescript/simple-mercari-web/src/components/ItemList.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Item, fetchItems } from '~/api';
+import { Item, fetchItems, deleteItem, fetchSearchItems } from '~/api';
 
 // const PLACEHOLDER_IMAGE = import.meta.env.VITE_FRONTEND_URL + '/logo192.png';
 
@@ -10,6 +10,9 @@ interface Prop {
 
 export const ItemList = ({ reload, onLoadCompleted }: Prop) => {
   const [items, setItems] = useState<Item[]>([]);
+  const [searchObj, setSearchObj] = useState<{name: string}>({name: ""});
+  const [searchItems, setSearchItems] = useState<Item[]>([]);
+
   useEffect(() => {
     // fetchData関数の定義
     const fetchData = () => {
@@ -30,24 +33,66 @@ export const ItemList = ({ reload, onLoadCompleted }: Prop) => {
     }
   }, [reload, onLoadCompleted]);// reload, onLoadCompletedの値が変わるたびに実行する
 
+  const handleDelete = async (id: number) => {
+    deleteItem(id).then(response => {
+      if (!response.ok) {
+        throw new Error(`Failed to delete item ${id}`);
+      }
+      setItems(items.filter(item => item.id !== id));
+    }).catch(error => console.error("Error deleting item:", error));
+  };
+
+  const handleSearch = async (name: string) => {
+    fetchSearchItems(name).then( (data) => {
+      setSearchItems(data.items);
+      onLoadCompleted();
+    }).catch((error) => {
+      console.error('GET error:', error);
+    });
+  };
+
+  const onValueChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchObj(prev => ({
+      ...prev,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
   return (
-    <div className='itemlist-container'>
-      {items.map((item) => {
-        // specify image's url of the item
-        const imageUrl = import.meta.env.VITE_BACKEND_URL + "/image/" + item.image_name;
-        return (
-          <div key={item.id} className="ItemList">
-            {/* TODO: Task 2: Show item images */}
-            <img src={imageUrl} className='item-image'/>
-            <p className='name-category'>
-              <span >Name: {item.name}</span>
-              <br />
-              <span>Category: {item.category}</span>
-            
-            </p>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <div className='Search'>
+        <form>
+          <input 
+            type='text'
+            name='name'
+            placeholder='name'
+            value={searchObj.name}
+            onChange={onValueChange}/>
+          <button type='button' onClick={() => handleSearch(searchObj.name)}>Search</button>
+        </form>
+      </div>
+      <div className='itemlist-container'>
+        {items.map((item) => {
+          // specify image's url of the item
+          const imageUrl = import.meta.env.VITE_BACKEND_URL + "/image/" + item.image_name;
+          
+          return (
+            <div key={item.id} className="ItemList">
+              {/* TODO: Task 2: Show item images */}
+              <button className="delete-button" onClick={()=>handleDelete(item.id)}>🗑️</button>
+              <img src={imageUrl} className='item-image'/>
+              <p className='name-category'>
+                <span >Name: {item.name}</span>
+                <br />
+                <span>Category: {item.category}</span>
+              
+              </p>
+            </div>
+          );
+          }
+        )}
+      </div>
+    </>
+    
   );
 };
